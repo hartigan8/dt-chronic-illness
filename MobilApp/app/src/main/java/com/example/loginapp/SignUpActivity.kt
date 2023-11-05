24b8d6d6cfd8f8bbae71567b54962960c593ef28
Alper Şallı <alpersallidgb@gmail.com>
2023-11-06 01:47:26 +0300

    signup

--- MobilApp/app/src/main/java/com/example/loginapp/SignUpActivity.kt
+++ MobilApp/app/src/main/java/com/example/loginapp/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.example.loginapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
@@ -17,6 +18,7 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
     private val client = OkHttpClient()
 
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
