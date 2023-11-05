9b94a20ca4dd6a70c6d1d2b4ca229d69a2470298
hartiganm8 <m.can-yilmaz@outlook.com>
2023-11-06 00:25:46 +0300

    routing and backend connection
    backend connected but register part is not done. there are missing fields it will be handled later.

---
+++ WebApp/src/SuccessPage.js
@@ -0,0 +1,12 @@
+// SuccessPage.js
+import React from 'react';
+
+function SuccessPage() {
+  return (
+    <div>
+      <h1>Logged in successfully!</h1>
+    </div>
+  );
+}
+
+export default SuccessPage;
